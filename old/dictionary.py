79048970531c97036e1cacc2086612b09bda3e81
Tiago Amaral <tiagoamaralc@gmail.com>
2019-12-22 00:24:45 -0300

    Second commit

---
+++ old/dictionary.py
@@ -0,0 +1,6 @@
+diasDaSemana = {1: "Domingo", 2: "Segunda", 3: "Terça", 4: "Quarta", 5: "Quinta", 6: "Sexta"}
+print(diasDaSemana)
+diasDaSemana[7] = "Sábado"
+print(diasDaSemana)
+print(diasDaSemana[3] #Retorna o valor da chave 3
+
---
+++ old/laco.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um número: "))
+if(a < 10):
+    print("O valor de a é menor que 10")
+elif(a == 10):
+    print("O valor de a é igual a 10")
+else:
+        print("O valor de a é maior que 10")
+

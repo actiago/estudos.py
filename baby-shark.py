74fcbf306040d66705ae8ab4eae6b2ef9967608b
Tiago Amaral <tiago.amaral@g10.com.br>
2020-02-02 11:53:56 -0300

    Novos scripts

---
+++ baby-shark.py
@@ -0,0 +1,6 @@
+"Baby shark song in python 3!"
+
+for name in ['Baby', 'Daddy', 'Mommy', 'Grandma', 'Grandpa']:
+    for _ in range(3):
+        print(f'{name} shark' + ' doo' * 6)
+    print(f'{name} shark!')

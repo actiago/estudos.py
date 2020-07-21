4135d88e8b14748b51af8b3239f7767e9d98aee5
Tiago Amaral <tiagoamaralc@gmail.com>
2020-07-21 13:50:01 -0300

    Tentando o ex 3.3.1

--- think-python/ex_3_3_1.py
+++ think-python/ex_3_3_1.py
@@ -8,3 +8,11 @@ com espaços suficientes à frente para que a última letra da string esteja na 
 
 Dica: Use concatenação de strings e repetição. Além disso, o Python oferece uma função integrada chamada len,
  que apresenta o comprimento de uma string, então o valor de len('monty') é 5."""
+
+s = str(input('Digite uma palavra aleatória: \n'))
+
+def right_justify(s):
+    size = len(s)
+    print(sep=' '*(70 - size) + s)
+
+print(right_justify)

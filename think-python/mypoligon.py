fb4ae924875e497bfaa5eb8a5493ec0df8785168
Tiago Amaral <tiagoamaralc@gmail.com>
2020-07-22 13:19:04 -0300

    Iniciando o cap 4

---
+++ think-python/mypoligon.py
@@ -0,0 +1,13 @@
+import turtle
+bob = turtle.Turtle()
+colors = [ 'red', 'green' , 'yellow', 'blue']
+turtle.bgcolor('black')
+print(bob)
+
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
+
+turtle.mainloop()
+
+tutle.done
